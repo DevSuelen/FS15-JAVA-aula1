52464459149265e1cc2b61dbaeeb0d900233f06e
DevSuelen <117091949+DevSuelen@users.noreply.github.com>
2023-01-09 09:56:04 -0300

    aula 1 de programação

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("referencia").innerHTML= "<h1>Olá, eu sou </h1>"; // Injeta codigo no index.html;
+console.log("No console"); // Imprime no console do inspecionar elemento
+// alert("Olá Mundo"); //Faz aparecer um poup na tela
